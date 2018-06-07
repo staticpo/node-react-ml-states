5eb7d7d85a327c0a1cd92c47f043a9aab3b5407c
Jorge Vazquez <jorge.vazquez@mercadolibre.com>
2018-06-07 16:39:41 -0300

    Integration complete.

--- client/src/App.js
+++ client/src/App.js
@@ -2,23 +2,124 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Tree } from 'antd';
+import ReactDOM from 'react-dom';
+
+
+const TreeNode = Tree.TreeNode;
+
+
 class App extends Component {
   state = {
-    response: ''
+    response: []
   };
   
   componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
+    this.getStates()
+
+      .then(res => {
+          //console.log(res.express);
+          this.setState({ response: res.express });
+        }
+      )
       .catch(err => console.log(err));
   }
   
-  callApi = async () => {
-    const response = await fetch('/api/hello');
+  getStates = async () => {
+    const response = await fetch('/BR');
     const body = await response.json();
     if (response.status !== 200) throw Error(body.message);
     return body;
   };
+
+  getCities = async (id) => {
+    const response = await fetch('/cities/' + id);
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    return body;
+  };
+
+  getNeighborhoods = async (id) => {
+    const response = await fetch('/neighborhoods/' + id);
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    return body;
+  };
+
+
+
+  onLoadData = (treeNode) => {
+    return new Promise((resolve) => {
+      if (treeNode.props.children) {
+        resolve();
+        return;
+      }
+
+      console.log(treeNode.props.dataRef.type);
+
+      switch(treeNode.props.dataRef.type) {
+          case "State":
+              
+              //Get cities
+              this.getCities(treeNode.props.dataRef.id)
+                .then(res => {
+                    console.log(res.express);
+                    treeNode.props.dataRef.children = [];
+                    res.express.forEach(function(singleitem) {
+                        treeNode.props.dataRef.children.push(singleitem);
+                    });
+
+                    this.setState({
+                      response: [...this.state.response],
+                    });
+                    
+                    resolve();
+                  }
+                )
+                .catch(err => console.log(err));
+
+          break;
+          case "City":
+              //Get neighborhood
+
+              this.getNeighborhoods(treeNode.props.dataRef.id)
+                .then(res => {
+                    console.log(res.express);
+                    treeNode.props.dataRef.children = [];
+                    res.express.forEach(function(singleitem) {
+                        treeNode.props.dataRef.children.push(singleitem);
+                    });
+
+                    this.setState({
+                      response: [...this.state.response],
+                    });
+                    
+                    resolve();
+                  }
+                )
+                .catch(err => console.log(err));
+          break;
+          default:
+              //?? Error ??
+              resolve();
+      }
+    });
+  };
+
+  renderTreeNodes = (data) => {
+    return data.map((item) => {
+      if (item.children) {
+        return (
+          <TreeNode title={item.name} key={item.id} dataRef={item}>
+            {this.renderTreeNodes(item.children)}
+          </TreeNode>
+        );
+      }
+      return (
+          <TreeNode title={item.name} key={item.id} dataRef={item}></TreeNode>
+      );
+    });
+  }
 
   render() {
     return (
@@ -27,7 +128,12 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">{this.state.response}</p>
+        
+        <Tree showLine loadData={this.onLoadData}>
+          {this.renderTreeNodes(this.state.response)}
+        </Tree>
+
+        
       </div>
     );
   }
--- server.js
+++ server.js
@@ -2,13 +2,17 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8081;
 
+var http = require('http');
+var https = require('https');
+
+
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
 
 
-app.get('/state/:id', function (req, res) {
+//app.get('/state/:id', function (req, res) {
    
 	/*
 
@@ -34,81 +38,12 @@ app.get('/state/:id', function (req, res) {
 		      "name": "Barra de Valizas"
 		    },
 		    {
-		      "id": "TUxVQ0JBUjE4NWNj",
-		      "name": "Barra del Chuy"
-		    },
-		    {
-		      "id": "TUxVQ0NBQjY1MmQ1",
-		      "name": "Cabo Polonio"
-		    },
-		    {
-		      "id": "TUxVQ0NBUzQxYzlh",
-		      "name": "Castillos"
-		    },
-		    {
-		      "id": "TUxVQ0NIVThjNTFm",
-		      "name": "Chuy"
-		    },
-		    {
-		      "id": "TUxVQ0NPUzQzZDk0",
-		      "name": "Costa Azul"
-		    },
-		    {
-		      "id": "TUxVQ0VMUDM0OTI",
-		      "name": "El Palmar"
-		    },
-		    {
-		      "id": "TUxVQ0xBWmFlYzJh",
-		      "name": "La Coronilla"
-		    },
-		    {
-		      "id": "TUxVQ0xBWmYzMmU0",
-		      "name": "La Palma"
-		    },
-		    {
-		      "id": "TUxVQ0xBWmQ4YTc1",
-		      "name": "La Paloma"
-		    },
-		    {
-		      "id": "TUxVQ0xBWjQyNGEy",
-		      "name": "La Pedrera"
-		    },
-		    {
-		      "id": "TUxVQ0xBR2JkZGMy",
-		      "name": "Laguna de Rocha"
-		    },
-		    {
-		      "id": "TUxVQ0xBUzcyNDI",
-		      "name": "Las Garzas"
-		    },
-		    {
-		      "id": "TUxVQ0xBU2YyZDU1",
-		      "name": "Lascano"
-		    },
-		    {
-		      "id": "TUxVQ09UUjQxODQ",
-		      "name": "Otras"
-		    },
-		    {
-		      "id": "TUxVQ1BVTjE5ZTIw",
-		      "name": "Punta del Diablo"
-		    },
-		    {
-		      "id": "TUxVQ1JPQzFjOWE5",
-		      "name": "Rocha"
-		    },
-		    {
-		      "id": "TUxVQ1NBTmVhNWRk",
-		      "name": "Santa Teresa"
-		    },
-		    {
 		      "id": "TUxVQ1ZFTDhlZWM3",
 		      "name": "Velázquez"
 		    }
 		  ]
 		}
 
-	*/
 
    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
       var users = JSON.parse(data);
@@ -116,10 +51,159 @@ app.get('/state/:id', function (req, res) {
       console.log(user);
       res.end(JSON.stringify(user));
    });
+	*/
 
+	
+/*
+	fetch('https://api.mercadolibre.com/classified_locations/countries/BR', { method: 'GET', headers: headers })
+    	.then(res => res.json())
+    	.then(json => console.log(json));
+    	*/
+    	/*
+    fetch('https://api.twitter.com/1.1/help/tos.json', {
+	  method: 'GET',
+	  headers: {'Accept': 'application/json'}
+	}).then(response => {
+		
+		return response.json();
+	}).catch(err => {console.log(err);});
+*/
+/*
+	fetch('https://en.wikipedia.org/w/api.php?action=query&titles=Albert%20Einstein&prop=info&format=json&callback=foo')
+    	.then(res => { 
+    		return res.text();
+    	})
+    	.then(body => console.log("ASDQWEASDQWE" + body))
+    	.catch(err => {console.log(err);});
+*/
+/*
+	// Las opciones que seran usadas en la peticion 
+	var options = {
+	   host: 'localhost',
+	   port: '80',
+	   path: '/index.htm'  
+	};
+
+
+	// Hacemos la peticion al server.
+	var req = http.request(options, function(response) {
+	   
+		// Se actualiza el stream continuamente con datos
+		var body = '';
+		response.on('data', function(data) {
+			body += data;
+		});
+
+		response.on('end', function() {
+			// los datos se recibieron completos.
+			console.log(body);
+			res.send({ express: body });
+		});
+
+	});
+	req.end();
 
 
 });
+*/
+
+
+app.get('/BR', async (req, res, next) => {
+  try {
+  	//https://api.mercadolibre.com/classified_locations/countries/BR
+	var url = 'https://api.mercadolibre.com/classified_locations/countries/BR';
+	apiMeli(url, function(err, data) {
+		if (err) { // Check for the error and throw if it exists.
+			res.send({ express: err });
+		} 
+		else {
+			response = data.states;
+			response.forEach(function(singleitem) {
+			    singleitem.type = "State";
+			});
+
+			//console.log(response);
+			res.send({ express: response });
+		} 
+	});
+
+    
+  } catch (e) {
+    //this will eventually be handled by your error handling middleware
+    console.log(e);
+  }
+});
+
+
+//Get the cities of any given state
+app.get('/cities/:id', async (req, res, next) => {
+  try {
+  	//https://api.mercadolibre.com/classified_locations/states/id
+	var url = 'https://api.mercadolibre.com/classified_locations/states/' + req.params.id;
+	apiMeli(url, function(err, data) {
+		if (err) { // Check for the error and throw if it exists.
+			res.send({ express: err });
+		} 
+		else {
+			response = data.cities;
+			response.forEach(function(singleitem) {
+			    singleitem.type = "City";
+			});
+
+			//console.log(response);
+			res.send({ express: response });
+		}
+	});
+    
+  } catch (e) {
+    //this will eventually be handled by your error handling middleware
+    console.log(e);
+  }
+});
+
+
+//Get the neighborhoods of any given city
+app.get('/neighborhoods/:id', async (req, res, next) => {
+  try {
+  	//https://api.mercadolibre.com/classified_locations/cities/id
+	var url = 'https://api.mercadolibre.com/classified_locations/cities/' + req.params.id;
+	apiMeli(url, function(err, data) {
+		if (err) { // Check for the error and throw if it exists.
+			res.send({ express: err });
+		} 
+		else {
+			response = data.neighborhoods;
+			response.forEach(function(singleitem) {
+			    singleitem.type = "Neighborhoods";
+			});
+
+			//console.log(response);
+			res.send({ express: response });
+		}
+	});
+    
+  } catch (e) {
+    //this will eventually be handled by your error handling middleware
+    console.log(e);
+  }
+});
+
 
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+function apiMeli(url, callback) {
+	https.get(url, function (res) {
+		res.on('data', function (d) {
+			var json = JSON.parse(d);
+			callback(null, json);
+		});
+		
+		res.on('error', function (e) {
+			console.error(e);
+			callback("Error", null);
+		});
+	});
+}
+
